c3297f7989f7cb00efbe1ab824ee935d6a094cca
Nicolas Perrenoud <mail@nicu.ch>
2020-11-16 19:20:25 +0200

    App Name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title }} | GR3W</title>
+        <title>{{ $title }} | {{ config('app.name') }}</title>
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         @livewireStyles
