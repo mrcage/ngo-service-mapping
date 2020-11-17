04a16bd907cdd097351dbaebc8573e0dab3e235f
Nicolas Perrenoud <mail@nicu.ch>
2020-11-17 12:23:52 +0200

    Navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,29 +9,32 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         @livewireStyles
     </head>
-    <body class="d-flex flex-column h-100">
-        <main role="main" class="flex-shrink-0">
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container d-flex">
+                <a class="navbar-brand" href="/"><x-bi-signpost-split class="h3 p-0 m-0"/></a>
+                <span>
+                    @auth
+                        <a href="{{ route('user-profile-information') }}">{{ Auth::user()->name }}</a> |
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    @endauth
+                    @guest
+                        <a href="{{ route('login') }}">Login</a> |
+                        <a href="{{ route('register') }}">Register</a>
+                    @endguest
+                </span>
+            </div>
+        </nav>
+        <main role="main">
             <div class="container my-3">
                 {{ $slot }}
             </div>
         </main>
-        <footer class="footer mt-auto py-3 bg-light">
-            <div class="container text-center">
-                @auth
-                    Logged in as <a href="{{ route('user-profile-information') }}">{{ Auth::user()->name }}</a> |
-                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        Logout
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                @endauth
-                @guest
-                    <a href="{{ route('login') }}">Login</a> |
-                    <a href="{{ route('register') }}">Register</a>
-                @endguest
-            </div>
-          </footer>
         <script src="{{ asset('js/app.js') }}" defer></script>
         @livewireScripts
     </body>
