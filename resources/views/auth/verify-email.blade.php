04435ec25f41f390715d6f33b916308ddaf5c982
Nicolas Perrenoud <mail@nicu.ch>
2020-11-18 14:06:37 +0200

    Update status message

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -8,9 +8,13 @@
         <x-bi-lock class="display-4 mb-4"/>
         <h1 class="h3 mb-3 font-weight-normal">Verify e-mail address</h1>
         @if (session('status'))
-            <x-alert type="success" :message="session('status')"/>
+            @php
+                $status = session('status') == 'verification-link-sent' ? 'The verification link has been sent.' : session('status');
+            @endphp
+            <x-alert type="success" :message="$status"/>
+        @else
+            <x-alert type="info" message="Please click the e-mail verification link that was sent to your e-mail address."/>
         @endif
-        <x-alert type="info" message="Please click the e-mail verification link that was sent to your e-mail address."/>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Send e-mail again</button>
         <p class="mt-5 mb-3 text-muted">
             <a href="{{ url()->previous() }}">Back to previous page</a>
