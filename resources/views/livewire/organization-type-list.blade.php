36632f28cceac293f7eb8c9036b48a466321f204
Nicolas Perrenoud <mail@nicu.ch>
2020-11-19 23:21:31 +0200

    Padding

--- resources/views/livewire/organization-type-list.blade.php
+++ resources/views/livewire/organization-type-list.blade.php
@@ -1,7 +1,7 @@
 <div>
     <h2>Organization Types</h2>
     @if($types->isNotEmpty())
-        <div class="list-group">
+        <div class="list-group mt-3">
             @foreach ($types as $type)
                 <a href="{{  route('types.show', $type) }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
                     <span>{{ $type->name }}</span>
--- resources/views/livewire/sector-list.blade.php
+++ resources/views/livewire/sector-list.blade.php
@@ -1,7 +1,7 @@
 <div>
     <h2>Sectors</h2>
     @if($sectors->isNotEmpty())
-        <div class="list-group">
+        <div class="list-group mt-3">
             @foreach ($sectors as $sector)
                 <a href="{{  route('sectors.show', $sector) }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
                     <span>{{ $sector->name }}</span>
