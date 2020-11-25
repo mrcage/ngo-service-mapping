11a8dda9c2ec0862cd387008954282ca0ebbc598
Nicolas Perrenoud <mail@nicu.ch>
2020-11-26 00:42:35 +0200

    Map fit bounds

--- resources/views/livewire/location-map.blade.php
+++ resources/views/livewire/location-map.blade.php
@@ -19,7 +19,7 @@
                 var maxLat = markers.reduce((a, b) => Math.max(a, b.getLatLng().lat), -90);
                 var minLng = markers.reduce((a, b) => Math.min(a, b.getLatLng().lng), 180);
                 var maxLng = markers.reduce((a, b) => Math.max(a, b.getLatLng().lng), -180);
-                map.flyToBounds([ // fitBounds
+                map.fitBounds([ // flyToBounds
                     [maxLat, minLng],
                     [minLat, maxLng]
                 ])
